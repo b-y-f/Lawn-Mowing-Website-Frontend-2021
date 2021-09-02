7ee83d2412708f3ec04f6d3c4d2f3c67d4bd9bec
Yifan Bao <bbbbb8bbbbb@gmail.com>
2021-09-02 15:30:45 +1200

    enable debug tool for redux

--- src/store.js
+++ src/store.js
@@ -1,14 +1,18 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux'
 import noticeReducer from './reducers/noticeReducer'
 import thunk from 'redux-thunk'
+import userReducer from './reducers/loginReducer'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
 
 const reducer = combineReducers({
-  message: noticeReducer
+  message: noticeReducer,
+  user:userReducer
 })
 
 const store = createStore(
   reducer,
-  applyMiddleware(thunk)
+  composeWithDevTools(applyMiddleware(thunk))
 )
 
 export default store
